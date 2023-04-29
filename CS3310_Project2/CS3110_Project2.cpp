30d7101c21284c603f1bfd0687a883eecc69a87e
Benjamin Liu <benjaminliu456@gmail.com>
2023-04-28 18:39:52 -0700

    Added quick sort

--- CS3310_Project2/CS3110_Project2.cpp
+++ CS3310_Project2/CS3110_Project2.cpp
@@ -18,7 +18,8 @@ int findKmerge(vector<int>, int);
 void mergeSort(vector<int>&, int, int);
 void merge(vector<int>&, int, int, int);
 int findKquick(vector<int>, int);
-void quickSort();
+void quickSort(vector<int>&, int, int, int);
+int partition(vector<int>&, int, int);
 
 int main()
 {
@@ -33,12 +34,21 @@ int main()
     
     findKmerge(a, 3);
     findKquick(a, 3);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a.at(i) << " ";
+    }
 }
 
 int findKmerge(vector<int> a, int k)
 {
     mergeSort(a, 0, a.size() - 1);
-    cout << "The " << "kth (" << k << ") smallest element is " << a.at(k) << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a.at(i) << " ";
+    }
+    cout << "\nMerge sort found the kth (" << k << ") smallest element is " << a.at(k) << endl;
     return a.at(k);
 }
 
@@ -88,15 +98,55 @@ void merge(vector<int>& a, int low, int mid, int high)
     int k = 0;
     for (int l = low; l <= high; l++)
     {
-        cout << temp.at(k) << " ";
         a.at(l) = temp.at(k);
         k++;
     }
-    cout << endl;
 }
 
 int findKquick(vector<int> a, int k)
 {
+    quickSort(a, 0, a.size() - 1, k);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a.at(i) << " ";
+    }
+    cout << "\nQuick Sort found the kth (" << k << ") smallest element is " << a.at(k) << endl;
     return a.at(k);
 }
 
+void quickSort(vector<int>& a, int p, int q, int k)
+{
+    if (p < q)
+    {
+        int piv = partition(a, p, q);
+        if (piv == k)
+        {
+            return;
+        }
+        quickSort(a, p, piv - 1, k);
+        quickSort(a, piv + 1, q, k);
+    }
+}
+
+int partition(vector<int>& a, int p, int q)
+{
+    int pivVal = a.at(p);
+    int pivotPos = p;
+
+    for (int i = p + 1; i <= q; i++)
+    {
+        if (a.at(i) < pivVal)
+        {
+            pivotPos++;
+            int temp = a.at(i);
+            a.at(i) = a.at(pivotPos);
+            a.at(pivotPos) = temp;
+        }
+    }
+
+    int temp = a.at(p);
+    a.at(p) = a.at(pivotPos);
+    a.at(pivotPos) = temp;
+
+    return pivotPos;
+}
